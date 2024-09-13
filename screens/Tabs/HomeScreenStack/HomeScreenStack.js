01025bc3c69d89bd093e33e92732394050e3ef30
Prince-Sedem <sedemdev@gmail.com>
2024-09-13 12:49:11 +0100

    Update with biceps stack

--- screens/Tabs/HomeScreenStack/HomeScreenStack.js
+++ screens/Tabs/HomeScreenStack/HomeScreenStack.js
@@ -13,10 +13,12 @@ import Legs from './Legs';
 import Calfs from './Calfs';
 import Chest2 from './Chest2';
 import Abs2 from './Abs2';
+import Biceps2 from './Biceps2';
 
 const HomeStack = createNativeStackNavigator();
 const ChestStack = createNativeStackNavigator();
 const AbsStack = createNativeStackNavigator();
+const BicepsStack = createNativeStackNavigator();
 
 function HomeScreenStack() {
   return (
@@ -24,7 +26,7 @@ function HomeScreenStack() {
           <HomeStack.Screen name="Home" component={HomeScreen} />
           <HomeStack.Screen name="Chest" component={ChestScreenStack} />
           <HomeStack.Screen name="Abs" component={AbsScreenStack} />
-          <HomeStack.Screen name="Biceps" component={Biceps} />
+          <HomeStack.Screen name="Biceps" component={BicepsScreenStack} />
           <HomeStack.Screen name="Triceps" component={Triceps} />
           <HomeStack.Screen name="Back" component={Back} />
           <HomeStack.Screen name="Shoulders" component={Shoulders} />
@@ -57,6 +59,18 @@ function AbsScreenStack() {
           options={{headerShown: false}}
           />
         </AbsStack.Navigator>
+  )
+}
+function BicepsScreenStack() {
+  return (
+        <BicepsStack.Navigator>
+          <BicepsStack.Screen name="Biceps" component={Biceps} 
+          options={{headerShown: false}}
+          />
+          <BicepsStack.Screen name="Biceps2" component={Biceps2} 
+          options={{headerShown: false}}
+          />
+        </BicepsStack.Navigator>
   )
 }
 
