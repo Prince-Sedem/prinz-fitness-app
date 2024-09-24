1e9cadc7106bc92d15083f8a536ced5192d0e779
Prince-Sedem <sedemdev@gmail.com>
2024-09-24 12:52:05 +0100

    Add new Biceps screen

---
+++ screens/Tabs/HomeScreenStack/BicepsDetails/StandingDumbbellCurl.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function StandingDumbbellCurl() {
+  return (
+    <View>
+      <Text>StandingDumbbellCurl</Text>
+    </View>
+  )
+}
