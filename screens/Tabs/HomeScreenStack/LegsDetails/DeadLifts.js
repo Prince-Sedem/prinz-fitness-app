16337af61db34aef14eda3525683a4760252d8e6
Prince-Sedem <sedemdev@gmail.com>
2024-10-22 13:15:55 +0100

    Add new exercise screens

---
+++ screens/Tabs/HomeScreenStack/LegsDetails/DeadLifts.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function DeadLifts() {
+  return (
+    <View>
+      <Text>DeadLifts</Text>
+    </View>
+  )
+}
