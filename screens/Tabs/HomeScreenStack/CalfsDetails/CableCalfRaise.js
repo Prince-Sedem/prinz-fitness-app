9147e2c2e031cb4f211ffe51cde3844377b2bf26
Prince-Sedem <sedemdev@gmail.com>
2024-10-23 13:45:29 +0100

    Add new exercise screen

---
+++ screens/Tabs/HomeScreenStack/CalfsDetails/CableCalfRaise.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CableCalfRaise() {
+  return (
+    <View>
+      <Text>CableCalfRaise</Text>
+    </View>
+  )
+}
